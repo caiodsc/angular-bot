cf0fb879ea7ab91f85e7fcc7870975e513544ad8
Caio <caio.dscamara@gmail.com>
2018-07-12 16:40:40 -0400

    Retirando da pasta

---
+++ angular-forms/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MaterializeModule } from 'ng2-materialize';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ChartsModule } from 'ng2-charts';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {FormsModule} from "./forms/forms.module";
+import {AnswersModule} from "./answers/answers.module";
+
+
+import { RouterModule } from '@angular/router';
+import { routing } from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule.forRoot(),
+    ColorPickerModule,
+    ChartsModule,
+    FormsModule,
+    AnswersModule,
+    RouterModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
